194cd269ca89c81c871f569c8ceefa55001d66ab
pat1543 <pat1543@gmail.com>
2019-08-24 19:59:00 +0900

    Add: kaminariを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,11 @@
 
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-
+  PER = 15
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).per(PER)
   end
 
   # GET /books/1
