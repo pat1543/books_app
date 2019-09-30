2b64179ba6d03ad5e2d846eeccced565497f6fee
pat1543 <pat1543@gmail.com>
2019-09-30 14:37:03 +0900

    Fix: モデルの名前を変更

--- app/controllers/relatives_controller.rb
+++ app/controllers/relatives_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class FollowRelationshipsController < ApplicationController
+class RelativesController < ApplicationController
   before_action :user_signed_in?
 
   def create
@@ -10,7 +10,7 @@ class FollowRelationshipsController < ApplicationController
   end
 
   def destroy
-    user = FollowRelationship.find(params[:id]).followed
+    user = Relative.find(params[:id]).followed
     current_user.unfollow(user)
     redirect_to user
   end
