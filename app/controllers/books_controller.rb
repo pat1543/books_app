c5abf08b13920ba23bb8386c47eb6ebbed3513e8
pat1543 <pat1543@gmail.com>
2019-11-04 17:28:49 +0900

    Fix: 不要なコメントを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,30 +3,24 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy] # GET /books
-  # GET /books.json
+
   def index
     @books = Book.page(params[:page])
   end
 
-  # GET /books/1
-  # GET /books/1.json
   def show
     @commentable = @book
     @comments = @commentable.comments
     @comment = Comment.new
   end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
   def edit
   end
 
-  # POST /books
-  # POST /books.json
   def create
     @book = current_user.books.build(book_params)
 
@@ -38,8 +32,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
   def update
     if @book.update(book_params)
       flash[:notice] = t("books.update.success")
@@ -49,8 +41,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     @book.destroy
     flash[:notice] = t("books.destroy.success")
@@ -58,12 +48,10 @@ class BooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_book
       @book = Book.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
       params.require(:book).permit(:title, :memo, :author, :picture)
     end
