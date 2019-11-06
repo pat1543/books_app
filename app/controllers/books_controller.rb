6ae54d25999d897ebd21dced8ed82090b118729b
pat1543 <pat1543@gmail.com>
2019-11-06 22:10:02 +0900

    Remove: コメントを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@
 
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy] # GET /books
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @books = Book.page(params[:page])
@@ -48,11 +48,12 @@ class BooksController < ApplicationController
   end
 
   private
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    def book_params
-      params.require(:book).permit(:title, :memo, :author, :picture)
-    end
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :memo, :author, :picture)
+  end
 end
